ef37e8ed865ecb911bc3ecb2b3fb9b4c850eb5f8
Rose <91130644+RoseDeLaAnne@users.noreply.github.com>
2023-10-11 18:11:52 +0300

    Update Login.tsx

--- frontend/src/pages/authentication/Login.tsx
+++ frontend/src/pages/authentication/Login.tsx
@@ -20,7 +20,8 @@ const App: React.FC = () => {
 
   const onFinish = async (values: any) => {
     try {
-      const response = await token(values);
+      // const response = await token(values);
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
       if (response.status === 200) {
         localStorageSetItem(response.data);
         // navigate("/users", { replace: true });
